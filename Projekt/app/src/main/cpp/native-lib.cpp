dead6f319e7f87bde70dfb55ae68d9cb95bdc030
Mziobrok <mziobrok@gmail.com>
2019-09-30 16:20:55 +0200

    Test pracy akcelerometru
    Programu przywoluje funkcje w C++ do pracy z akcelerometrem. Interfejs tworzony w Javie przywoluje funkcje C++ prze JNI.

--- Projekt/app/src/main/cpp/native-lib.cpp
+++ Projekt/app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,54 @@
 #include <jni.h>
 #include <string>
+#include <android/sensor.h>
+#include <android/log.h>
+#include <android/looper.h>
+#include <unistd.h>
+#define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, "MainActivity", __VA_ARGS__))
+#define LOGW(...) ((void)__android_log_print(ANDROID_LOG_WARN, "MainActivity", __VA_ARGS__))
+#define ourID 3
+ASensorEventQueue* eventQ;
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_example_projekt_MainActivity_stringFromJNI(
-        JNIEnv* env,
+extern "C" {
+JNIEXPORT void JNICALL
+Java_com_example_projekt_MainActivity_Ini(
+        JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+    ALooper *thr = ALooper_prepare(ALOOPER_PREPARE_ALLOW_NON_CALLBACKS); //zwraca referencje do loopera tego watku lub tworzy nowy gdy go brak
+    ALooper_acquire(thr); //zabrania usunieciu loopera, w tym przypadku wydaje sie by nie potrzebne
+    ASensorManager *sensorManager = ASensorManager_getInstance(); // referencja do obiektu managera
+    const ASensor *acc = ASensorManager_getDefaultSensor(
+            sensorManager,
+            ASENSOR_TYPE_ACCELEROMETER); //referencja do akcelerometru
+    eventQ = ASensorManager_createEventQueue(
+            sensorManager,
+            thr, ourID,
+            NULL, NULL);//tworzymy kolejke eventow dla naszego looper, 4 argument jest funkcja automatycznie wywolywana jesli looper zbierze event z sensora, tutaj brak jako ze robimy to recznie z javy
+    ASensorEventQueue_enableSensor(eventQ,
+                                   acc);
+    ASensorEventQueue_setEventRate(eventQ,
+                                   acc,
+                                   100000); //10hz, okres probkowania w mikrosekundach jako argument
+
+}
+JNIEXPORT jstring JNICALL
+        Java_com_example_projekt_MainActivity_Update(
+                JNIEnv *env,
+                jobject /* this */
+                )
+{
+   // ALooper_pollAll(0, NULL, NULL, NULL);
+    ASensorEvent event;
+    std::string hello;
+    //odczytuje wszystkie zebrane eventy i bierze najnowszego na wyjscie (ostatnieg)
+    while (ASensorEventQueue_getEvents(eventQ, &event, 1) > 0){ //odbieramy event
+        __android_log_print(ANDROID_LOG_INFO, "MainActivity", "accelerometer: x=%f y=%f z=%f",
+                            event.acceleration.x, event.acceleration.y,
+                            event.acceleration.z);
+        hello = "accelerometer: x=" + std::to_string(event.acceleration.x) + ", y=" +
+                            std::to_string(event.acceleration.y) + ", z=" +
+                            std::to_string(event.acceleration.z);}
+        return env->NewStringUTF(hello.c_str());
+
+}
 }
--- Projekt/app/src/main/java/com/example/projekt/MainActivity.java
+++ Projekt/app/src/main/java/com/example/projekt/MainActivity.java
@@ -3,24 +3,37 @@ package com.example.projekt;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
-
+import android.os.Handler;
+import android.os.Message;
 public class MainActivity extends AppCompatActivity {
+    private static final int DISPLAY_DATA = 1;
+    private static TextView tv;
+    private static int hr = 0;
+    // this handler will receive a delayed message
+    private Handler mHandler = new Handler() {
 
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what == DISPLAY_DATA)  tv = findViewById(R.id.sample_text);
+            tv.setText(Update());
+            mHandler.sendEmptyMessageDelayed(DISPLAY_DATA, 100);
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-    // Example of a call to a native method
-    TextView tv = findViewById(R.id.sample_text);
-    tv.setText(stringFromJNI());
+        Ini();
+        mHandler.sendEmptyMessageDelayed(DISPLAY_DATA, 100);
     }
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
+    public native String Update();
+    public native void Ini();
 
     // Used to load the 'native-lib' library on application startup.
     static {
