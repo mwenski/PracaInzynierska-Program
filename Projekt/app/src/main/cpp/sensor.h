2e34188e6258225ee2db58742470a8341cf9ab1a
piotr1202 <55048144+piotr1202@users.noreply.github.com>
2019-11-02 14:48:04 +0100

    Update sensor.h

--- Projekt/app/src/main/cpp/sensor.h
+++ Projekt/app/src/main/cpp/sensor.h
@@ -11,7 +11,12 @@
 #include "glm/gtx/rotate_vector.hpp"
 #include "glm/gtx/closest_point.hpp"
 #include "glm/vec3.hpp"
-void initialization(char flags);
-glm::vec3 accelGet();
-glm::vec3 accelGet();
+void initialization_acceleration(char flags);
+void initialization_gyroscope(char flags);
+void initialization_rotation(char flags);
+void initialization_magnetic(char flags);
+glm::vec3 accelGet1();
+glm::vec3 accelGet2();
+glm::vec3 accelGet3();
+glm::vec3 accelGet4();
 #endif //PROJEKT_SENSOR_H
